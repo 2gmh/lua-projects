8ab3fd558911e206049dc3ad9e9306cc7f987165
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 15:47:25 -0300

    Create change-materials.lua

---
+++ change-materials.lua
@@ -0,0 +1,12 @@
+print("Procesando")
+wait(4)
+objeto = script.Parent.changer
+-- Cambia el color.
+objeto.BrickColor = BrickColor.new("Lime green")
+-- Cambia el material.
+objeto.Material = "Neon"
+-- Cambia el tamaño
+objeto.Size = Vector3.new(5, 3, 1)
+print("Se convirtio a verde.")
+print("Se convirtio en Neon")
+print("Ya cambio el tamaño")
