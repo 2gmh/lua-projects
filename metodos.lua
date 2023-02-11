b26c9b2d3e2cb657fa9ffc615c46e897f5e46fe9
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 16:50:55 -0300

    Create metodos.lua

---
+++ metodos.lua
@@ -0,0 +1,10 @@
+--Destroy() --destruir()
+--Clone()
+--ClearAllChildren()
+
+local clon = game.Workspace.metodos:Clone()
+clon.Parent = game.Workspace
+wait(5)
+game.Workspace.metodos:ClearAllChildren()
+
+
