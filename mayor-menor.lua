de5726a1459493e563d17a1e672da922307446dc
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 15:13:43 -0300

    Create mayor-menor.lua

---
+++ mayor-menor.lua
@@ -0,0 +1,9 @@
+numero1 = 60
+numero2 = 14
+if numero2 < numero1 then
+	print(numero2, "Es menor que", numero1)
+elseif numero1 > numero2 then
+	print(numero1 "Es mayor que", numero2)
+else
+	print(numero1, "y", numero2, "son iguales.")
+end
