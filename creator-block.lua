538fa69d30c7a4656436dac893ae783cd1f3287d
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 16:04:55 -0300

    Create creator-block.lua

---
+++ creator-block.lua
@@ -0,0 +1,5 @@
+local myPart = Instance.new("Part", game.Workspace)
+myPart.Name = 'Mi bloque'
+myPart.BrickColor = BrickColor.new("Really blue")
+myPart.Position = Vector3.new(50, 50, 50)
+myPart.Anchored = true
