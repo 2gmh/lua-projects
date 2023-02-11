d719f2b18c86be486a6098559041d6052cdbc75a
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 16:14:16 -0300

    Create block-random-fuction.lua

---
+++ block-random-fuction.lua
@@ -0,0 +1,7 @@
+function crearPartes()
+	local bloquesito = Instance.new("Part", game.Workspace)
+	bloquesito.Name = "Mi bloquesito"
+	bloquesito.BrickColor = BrickColor.Random()
+	bloquesito.Transparency = 0.5
+end
+crearPartes()
