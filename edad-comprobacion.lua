9a038fd493616aad7362d22b8a45ef9621f8b7ec
faso <83717613+2gmh@users.noreply.github.com>
2023-02-11 14:56:50 -0300

    Create edad-comprobacion.lua

---
+++ edad-comprobacion.lua
@@ -0,0 +1,10 @@
+-- if condicion es verdadera then
+--     se ejecuta el codigo
+-- end
+
+valor_usuario = 19
+if valor_usuario >=18 then
+	print("Eres mayor de edad")
+else 
+	print("No eres mayor.")
+end
